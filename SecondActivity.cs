7620dca02f38131dd34b14b544c57c6bca2638a9
TaurenArrow <64417845+TaurenArrow@users.noreply.github.com>
2021-07-01 12:30:23 +0200

    Add files via upload

---
+++ SecondActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+using static Android.Resource;
+
+namespace MorePages
+{
+    [Activity(Label = "Second Page", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class SecondActivity : AppCompatActivity
+    {
+        Button btnSecond;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
+            // Create your application here
+            SetContentView(Resource.Layout.second_page);
+
+            btnSecond = FindViewById<Button>(Resource.Id.btnSecondPage);
+
+            btnSecond.Click += delegate { StartActivity(typeof(MainActivity)); };
+        }
+    }
+}
